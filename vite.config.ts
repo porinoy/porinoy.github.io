272003bfdc4c661254b3d31531a9edd39a5bc9c4
porinoy <chiranjoy@zohomail.in>
2026-07-10 20:20:39 +0530

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-
-import Inspector from 'vite-plugin-vue-inspector'
-
-export default defineConfig({
-  plugins: [Vue(), Inspector()],
-  base: '/',
-})
